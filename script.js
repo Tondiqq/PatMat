b5b054af4a4c36d79bc6edf8d0b9a556db7caebd
Tondiqq <antoninstus@gmail.com>
2025-01-20 09:00:32 +0100

    ukol2

--- script.js
+++ script.js
@@ -9,11 +9,12 @@ const mat = "<img src='img/mat.png'>";
 
 let kodFigurek = "";
 let i=0;
-while (i<10) {
+while (i<5) {
   i++;
   kodFigurek += "<div>" + pat + mat + "</div>";
+  kodFigurek += "<div>" + mat + pat + "</div>";
 }
 figurky.innerHTML = kodFigurek; // vloží kód řady obrázků, kterou požadujeme vykreslit
-pocetPat.innerText = i;         // vypíše počet vykreslených figurek Pata (všech deset)
-pocetMat.innerText = i;         // vypíše počet vykreslenách figurek Mata (žádný)
+pocetPat.innerText = i*2;         // vypíše počet vykreslených figurek Pata (všech deset)
+pocetMat.innerText = i*2;         // vypíše počet vykreslenách figurek Mata (žádný)
 
